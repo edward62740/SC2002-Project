a91b7e9c5e8cde1ff1be605174f5e81f5cf7a387
ChiangYuHsuan <148747135+ChiangYuHsuan@users.noreply.github.com>
2023-11-11 06:35:42 +0800

    Update CSV related codes
    Reading and Writing of Student and Camp summaries are both under 1 class
    
    Created CreateIndividual that will create the individual CSV representing student and camp details

--- CSV/mainClass.java
+++ CSV/mainClass.java
@@ -1,9 +1,20 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class mainClass {
 
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        System.out.println(StudentCSV.readFile());
+        System.out.println(ReadCSV.readFile("Data/camp_list.csv"));
+
+        List<List<String>> test = new ArrayList<>();
+
+        test.add(List.of("1", "2"));
+        test.add(List.of("Emilia's Camp", "Miku's Camp"));
+
+        ReadCSV.writeFile("Data/camp_summary.csv", test);
+
+        CreateIndividual.CreateIndividual("Data/camp_list.csv", "CAMP");
     }
 }
